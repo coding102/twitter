dd912235149754e4fafc58b7780f3b848ceeffb4
Marck <mercercountynj@gmail.com>
2016-04-04 20:47:52 -0400

    added tweetlist and materialize

---
+++ app/assets/frontend/components/TweetsList.jsx
@@ -0,0 +1,21 @@
+export default class TweetsList extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul className="collection">
+                    <li className="collection-item avatar">
+                        <i className="material-icons circle">person_pin</i>
+                        <span className="title">Samer Buna</span>
+                        <p>My #FirstTweet</p>
+                    </li>
+
+                    <li className="collection-item avatar">
+                        <i className="material-icons circle">person_pin</i>
+                        <span className="title">Samer Buna</span>
+                        <p>My #FirstTweet</p>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
--- config/application.rb
+++ config/application.rb
@@ -24,3 +24,5 @@ module TwitterClone
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+
