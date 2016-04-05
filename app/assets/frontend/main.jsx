42b8e4ba62a75cd3c8745db81ca59c41eb668f38
Marck <mercercountynj@gmail.com>
2016-04-04 21:06:44 -0400

    added tweet component and hard coded tweet

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,12 +1,22 @@
 import TweetBox from "./components/TweetBox"
 import TweetsList from "./components/TweetsList";
 
+// hard-coding tweets
+let mockTweets = [
+    { id: 1, name: 'Samer Buna', body: 'My #FirstTweet' },
+    { id: 2, name: 'Samer Buna', body: 'My #SecondTweet' },
+    { id: 3, name: 'Samer Buna', body: 'My #ThirdTweet' },
+];
+
+
+
+
 class Main extends React.Component{
     render() {
         return (
             <div className="container">
                 <TweetBox />
-                <TweetsList />
+                <TweetsList tweets={mockTweets} />
             </div>
         );
     }
