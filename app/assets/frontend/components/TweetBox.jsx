fe42839db8b2449a4a37a7d9ed14b74fc0ee0e40
Marck <mercercountynj@gmail.com>
2016-04-06 17:48:18 -0400

    add tweets in memory only

--- app/assets/frontend/components/TweetBox.jsx
+++ app/assets/frontend/components/TweetBox.jsx
@@ -1,10 +1,16 @@
 export default class TweetBox extends React.Component {
+    sendTweet(event) {
+        event.preventDefault();
+        this.props.sendTweet(this.refs.tweetTextArea.value);
+        this.refs.tweetTextArea.value = '';
+    }
+
     render () {
         return (
             <div className="row">
-                <form>
+                <form onSubmit={this.sendTweet.bind(this)}>
                     <div className="input-field">
-                        <textarea className="materialize-textarea" />
+                        <textarea ref="tweetTextArea" className="materialize-textarea" />
                         <label>What's happening?</label>
                         <button className="btn right">Tweet</button>
                     </div>
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,22 +1,22 @@
 import TweetBox from "./components/TweetBox"
 import TweetsList from "./components/TweetsList";
 
-// hard-coding tweets
-let mockTweets = [
-    { id: 1, name: 'Samer Buna', body: 'My #FirstTweet' },
-    { id: 2, name: 'Samer Buna', body: 'My #SecondTweet' },
-    { id: 3, name: 'Samer Buna', body: 'My #ThirdTweet' },
-];
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { tweetsList: [] };
+    }
+    addTweet(tweetToAdd) {
+        let newTweetsList = this.state.tweetsList;
+        newTweetsList.unshift({ id: Date.now(), name: 'Guest', body: tweetToAdd });
 
-
-
-
-class Main extends React.Component{
+        this.setState({ tweetsList: newTweetsList });
+    }
     render() {
         return (
             <div className="container">
-                <TweetBox />
-                <TweetsList tweets={mockTweets} />
+                <TweetBox sendTweet={this.addTweet.bind(this)} />
+                <TweetsList tweets={this.state.tweetsList} />
             </div>
         );
     }
