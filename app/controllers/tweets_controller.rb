b1fa0b158d75fe68dd72ff521fb476f6f8412737
Marck <mercercountynj@gmail.com>
2016-04-06 21:54:33 -0400

    added gravitar, timeago, rails g resource tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,12 @@
 class TweetsController < ApplicationController
 
+  # avoid n+1, avoid sql query of every object in the collection >> by adding includes call
   def index
-    render json: Tweet.all
+    render json: Tweet.includes(:user).order(created_at: :desc).all
   end
 
   def create
-    tweet = Tweet.create(body: params[:tweet], user_id: current_user.id)
+    tweet = Tweet.create(body: params[:body], user_id: current_user.id)
     render json: tweet
   end
 
